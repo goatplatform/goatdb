f97dd40121757c409d7adf8c70d3461503f19e93
Ofri Wolfus <ofri.wolfus@gmail.com>
2025-01-20 12:38:26 +0200

    Fixed: Head pointer not being updated properly causing non-linear history

--- db/managed-item.ts
+++ db/managed-item.ts
@@ -211,6 +211,7 @@ export class ManagedItem<S extends Schema = Schema> extends Emitter<'change'> {
     if (newHead) {
       this.rebase();
     }
+    this._head = newHead;
     this._commitInProgress = false;
   }
 
