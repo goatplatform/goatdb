1322a1784bccff2997ceab43afd48219e8f8b195
Ofri Wolfus <ofri.wolfus@gmail.com>
2025-01-20 16:24:45 +0200

    Updated schema scaffold

--- cli/init.ts
+++ cli/init.ts
@@ -96,24 +96,67 @@ const denoJsonScaffold = {
 
 const schemaScaffold = `import { SchemaManager } from '@goatdb/goatdb';
 
-export const kSchemaTask = {
-  ns: 'task',
+/**
+ * A schema defines the structure of items that can be stored in the DB.
+ * Adding a new schema involves these 3 steps:
+ *
+ * 1. Define a new const schema definition.
+ *    Tip: Remember the as const in the end.
+ *
+ *    export const kSchemaMyItem = {
+ *      ns: 'MyItem',
+ *      version: 1,
+ *      fields: {
+ *        title: {
+ *          type: 'string',
+ *          default: () => 'Untitled',
+ *        },
+ *        value: {
+ *          type: 'number',
+ *          required: true,
+ *        },
+ *      },
+ *    } as const;
+ *
+ * 2. Define a utility type for this const.
+ *
+ *    export type SchemaMyItem = typeof kSchemaMyItem;
+ *
+ * 3. Edit the registerSchemas() function at the bottom of this file and
+ *    include a call to manager.register().
+ *
+ *    manager.register(kSchemaMyItem);
+ */
+export const kSchemaMyItem = {
+  ns: 'MyItem',
   version: 1,
   fields: {
-    text: {
+    title: {
       type: 'string',
-      default: () => '',
+      default: () => 'Untitled',
     },
-    done: {
-      type: 'boolean',
-      default: () => false,
+    value: {
+      type: 'number',
+      required: true,
     },
   },
 } as const;
-export type SchemaTypeTask = typeof kSchemaTask;
+export type SchemaMyItem = typeof kSchemaMyItem;
 
-export function registerSchemas(): void {
-  SchemaManager.default.register(kSchemaTask);
+// ====== Add new schemas here ====== //
+
+/**
+ * This is the main registration function for all schemas in this project.
+ * It gets called from both the client and the server code so they agree on the
+ * same schemas.
+ *
+ * @param manager The schema manager to register with.
+ *                Uses {@link SchemaManager.default} if not provided.
+ */
+export function registerSchemas(
+  manager: SchemaManager = SchemaManager.default,
+): void {
+  manager.register(kSchemaMyItem);
 }
 `;
 
