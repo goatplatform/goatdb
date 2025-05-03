6430aab032af45587619382dd0e28e2c21a7da5c
Ofri Wolfus <ofri.wolfus@gmail.com>
2025-05-03 14:59:41 +0300

    Bundling order stamp

---
+++ tests/orderstamp-expose.test.ts
@@ -0,0 +1,35 @@
+import { assertTrue } from './asserts.ts';
+import { TEST } from './mod.ts';
+import { Orderstamp } from '../mod.ts';
+
+export default function setup(): void {
+  TEST('Orderstamp', 'Orderstamp API is exposed', () => {
+    assertTrue(typeof Orderstamp === 'object');
+    assertTrue(typeof Orderstamp.start === 'function');
+    assertTrue(typeof Orderstamp.end === 'function');
+    assertTrue(typeof Orderstamp.from === 'function');
+    assertTrue(typeof Orderstamp.between === 'function');
+  });
+
+  TEST('Orderstamp', 'Orderstamp basic usage', () => {
+    const s = Orderstamp.start();
+    const e = Orderstamp.end();
+    const f = Orderstamp.from(42);
+    const b = Orderstamp.between(s, e);
+    assertTrue(typeof s === 'string');
+    assertTrue(typeof e === 'string');
+    assertTrue(typeof f === 'string');
+    assertTrue(typeof b === 'string');
+    assertTrue(s < e);
+    assertTrue(s < b && b < e);
+  });
+
+  TEST('Orderstamp', 'Orderstamp between is monotonic', () => {
+    const s = Orderstamp.start();
+    const e = Orderstamp.end();
+    const b1 = Orderstamp.between(s, e);
+    const b2 = Orderstamp.between(s, b1);
+    const b3 = Orderstamp.between(b1, e);
+    assertTrue(s < b2 && b2 < b1 && b1 < b3 && b3 < e);
+  });
+}
--- tests/tests-entry.ts
+++ tests/tests-entry.ts
@@ -3,12 +3,14 @@ import { TestsRunner } from './mod.ts';
 import setupUntrusted from './db-untrusted.test.ts';
 import setupTrusted from './db-trusted.test.ts';
 import setupItemPath from './item-path.ts';
+import setupOrderstamp from './orderstamp-expose.test.ts';
 import { exit } from '../base/process.ts';
 
 async function main(): Promise<void> {
   setupUntrusted();
   setupTrusted();
   setupItemPath();
+  setupOrderstamp();
   await TestsRunner.default.run();
 
   if (!isBrowser()) {
