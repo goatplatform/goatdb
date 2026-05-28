d01c2c37c30b41e6a4edf6972eceeb654a4037cd
Ofri Wolfus <ofri.wolfus@gmail.com>
2026-05-28 10:58:53 +0300

    rename NODE_ONLY_FILTER_TEST to SHARED_SERVER_FILTER_TEST

--- tests/test-filter-constants.ts
+++ tests/test-filter-constants.ts
@@ -1,5 +1,5 @@
-export const NODE_ONLY_FILTER_TEST =
-  'createBuildContext fails clearly on Node.js';
+export const SHARED_SERVER_FILTER_TEST =
+  'createBuildContext bundles CSS on Deno and Node';
 export const DENO_ONLY_FILTER_TEST =
   'buildAssets with runtime: deno bundles CSS into /index.css';
 export const BROWSER_FAILURE_FILTER_TEST =
