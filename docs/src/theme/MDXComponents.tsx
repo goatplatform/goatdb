f58bcc78ad41e010466a03e6561740b4c8282d67
Ofri Wolfus <ofri.wolfus@gmail.com>
2026-08-07 17:07:07 +0300

    Site polish: apply sticky-navbar anchor offset class to MDX anchors

--- docs/src/theme/MDXComponents.tsx
+++ docs/src/theme/MDXComponents.tsx
@@ -1,10 +1,12 @@
 import type { ReactNode } from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
+import { useAnchorTargetClassName } from '@docusaurus/theme-common';
 import useBrokenLinks from '@docusaurus/useBrokenLinks';
 
 function Anchor({ id }: { id: string }): ReactNode {
   useBrokenLinks().collectAnchor(id);
-  return <a id={id} />;
+  const className = useAnchorTargetClassName(id);
+  return <a id={id} className={className} />;
 }
 
 export default {
